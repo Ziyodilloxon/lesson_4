b3a9941f2001c277ca70865366897d69125abb56
ziyodilloxon <ziyodilloxon8@gmail.com>
2024-05-31 22:17:43 +0500

    6-oy.4-dars uy ishi

---
+++ src/components/footer/footer.jsx
@@ -0,0 +1,14 @@
+import "./footer.css";
+
+function Footer() {
+  return (
+    <div className="footer">
+      <div className="footer-container">
+        <p>All rights reserved</p>
+        <a href="https://www.instagram.com/zikko_595/">Ziyodilloxon Valamatov</a>
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
